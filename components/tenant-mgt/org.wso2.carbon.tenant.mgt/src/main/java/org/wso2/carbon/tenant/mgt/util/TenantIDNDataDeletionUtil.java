cac9a4876e4ff802847a1c521b97a831cb9c7910
Thumilan Mikunthan <thumilan.15@cse.mrt.ac.lk>
2018-12-05 13:17:55 +0530

    DB queries organised

--- components/tenant-mgt/org.wso2.carbon.tenant.mgt/src/main/java/org/wso2/carbon/tenant/mgt/util/TenantIDNDataDeletionUtil.java
+++ components/tenant-mgt/org.wso2.carbon.tenant.mgt/src/main/java/org/wso2/carbon/tenant/mgt/util/TenantIDNDataDeletionUtil.java
@@ -20,6 +20,7 @@ package org.wso2.carbon.tenant.mgt.util;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -28,6 +29,20 @@ public class TenantIDNDataDeletionUtil {
 
     public static final Log log = LogFactory.getLog(TenantIDNDataDeletionUtil.class);
 
+    private static final String DELETE_CM_PURPOSE_CATEGORY = "DELETE FROM CM_PURPOSE_CATEGORY WHERE TENANT_ID = ?";
+    private static final String DELETE_IDN_CLAIM = "DELETE FROM IDN_CLAIM WHERE TENANT_ID = ?";
+    private static final String DELETE_IDN_CLAIM_DIALECT = "DELETE FROM IDN_CLAIM_DIALECT WHERE TENANT_ID = ?";
+    private static final String DELETE_IDN_CLAIM_MAPPED_ATTRIBUTE = "DELETE FROM IDN_CLAIM_MAPPED_ATTRIBUTE WHERE TENANT_ID = ?";
+    private static final String DELETE_IDN_CLAIM_MAPPING = "DELETE FROM IDN_CLAIM_MAPPING WHERE TENANT_ID = ?";
+    private static final String DELETE_IDN_CLAIM_PROPERTY = "DELETE FROM IDN_CLAIM_PROPERTY WHERE TENANT_ID = ?";
+    private static final String DELETE_IDN_OIDC_SCOPE = "DELETE FROM IDN_OIDC_SCOPE WHERE TENANT_ID = ?";
+    private static final String DELETE_IDP = "DELETE FROM IDP WHERE TENANT_ID = ?";
+    private static final String DELETE_IDP_AUTHENTICATOR = "DELETE FROM IDP_AUTHENTICATOR WHERE TENANT_ID = ?";
+    private static final String DELETE_IDP_AUTHENTICATOR_PROPERTY = "DELETE FROM IDP_AUTHENTICATOR_PROPERTY WHERE TENANT_ID = ?";
+    private static final String DELETE_IDP_METADATA = "DELETE FROM IDP_METADATA WHERE TENANT_ID = ?";
+    private static final String DELETE_IDP_PROVISIONING_CONFIG = "DELETE FROM IDP_PROVISIONING_CONFIG WHERE TENANT_ID = ?";
+    private static final String DELETE_IDP_PROV_CONFIG_PROPERTY = "DELETE FROM IDP_PROV_CONFIG_PROPERTY WHERE TENANT_ID = ?";
+
     /**
      * Delete all tenant information related to tenant stored in IDN tables
      *
@@ -35,55 +50,29 @@ public class TenantIDNDataDeletionUtil {
      * @param conn     database connection object
      * @throws SQLException thrown if an error occurs while executing the queries
      */
-    protected static void deleteTenantIDNData(int tenantId, Connection conn) throws Exception {
+    protected static void deleteTenantIDNData(int tenantId, Connection conn) throws SQLException {
 
         try {
             conn.setAutoCommit(false);
-            String deleteCM_PURPOSE_CATEGORYSql = "DELETE FROM CM_PURPOSE_CATEGORY WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteCM_PURPOSE_CATEGORYSql, tenantId);
-
-            String deleteIDN_CLAIMSql = "DELETE FROM IDN_CLAIM WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDN_CLAIMSql, tenantId);
-
-            String deleteIDN_CLAIM_DIALECTSql = "DELETE FROM IDN_CLAIM_DIALECT WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDN_CLAIM_DIALECTSql, tenantId);
-
-            String deleteIDN_CLAIM_MAPPED_ATTRIBUTESql = "DELETE FROM IDN_CLAIM_MAPPED_ATTRIBUTE WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDN_CLAIM_MAPPED_ATTRIBUTESql, tenantId);
-
-            String deleteIDN_CLAIM_MAPPINGSql = "DELETE FROM IDN_CLAIM_MAPPING WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDN_CLAIM_MAPPINGSql, tenantId);
-
-            String deleteIDN_CLAIM_PROPERTYSql = "DELETE FROM IDN_CLAIM_PROPERTY WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDN_CLAIM_PROPERTYSql, tenantId);
-
-            String deleteIDN_OIDC_SCOPESql = "DELETE FROM IDN_OIDC_SCOPE WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDN_OIDC_SCOPESql, tenantId);
-
-            String deleteIDPSql = "DELETE FROM IDP WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDPSql, tenantId);
-
-            String deleteIDP_AUTHENTICATORSql = "DELETE FROM IDP_AUTHENTICATOR WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDP_AUTHENTICATORSql, tenantId);
-
-            String deleteIDP_AUTHENTICATOR_PROPERTYSql = "DELETE FROM IDP_AUTHENTICATOR_PROPERTY WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDP_AUTHENTICATOR_PROPERTYSql, tenantId);
-
-            String deleteIDP_METADATASql = "DELETE FROM IDP_METADATA WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDP_METADATASql, tenantId);
-
-            String deleteIDP_PROVISIONING_CONFIGSql = "DELETE FROM IDP_PROVISIONING_CONFIG WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDP_PROVISIONING_CONFIGSql, tenantId);
-
-            String deleteIDP_PROV_CONFIG_PROPERTYSql = "DELETE FROM IDP_PROV_CONFIG_PROPERTY WHERE TENANT_ID = ?";
-            executeDeleteQuery(conn, deleteIDP_PROV_CONFIG_PROPERTYSql, tenantId);
-
+            executeDeleteQuery(conn, DELETE_CM_PURPOSE_CATEGORY, tenantId);
+            executeDeleteQuery(conn, DELETE_IDN_CLAIM, tenantId);
+            executeDeleteQuery(conn, DELETE_IDN_CLAIM_DIALECT, tenantId);
+            executeDeleteQuery(conn, DELETE_IDN_CLAIM_MAPPED_ATTRIBUTE, tenantId);
+            executeDeleteQuery(conn, DELETE_IDN_CLAIM_MAPPING, tenantId);
+            executeDeleteQuery(conn, DELETE_IDN_CLAIM_PROPERTY, tenantId);
+            executeDeleteQuery(conn, DELETE_IDN_OIDC_SCOPE, tenantId);
+            executeDeleteQuery(conn, DELETE_IDP, tenantId);
+            executeDeleteQuery(conn, DELETE_IDP_AUTHENTICATOR, tenantId);
+            executeDeleteQuery(conn, DELETE_IDP_AUTHENTICATOR_PROPERTY, tenantId);
+            executeDeleteQuery(conn, DELETE_IDP_METADATA, tenantId);
+            executeDeleteQuery(conn, DELETE_IDP_PROVISIONING_CONFIG, tenantId);
+            executeDeleteQuery(conn, DELETE_IDP_PROV_CONFIG_PROPERTY, tenantId);
             conn.commit();
         } catch (Exception e) {
             conn.rollback();
-            String errorMsg = "An error occurred while deleting registry data for tenant: " + tenantId;
+            String errorMsg = "An error occurred while deleting identity data for tenant: " + tenantId;
             log.error(errorMsg, e);
-            throw new Exception(errorMsg, e);
+            throw new SQLException(errorMsg, e);
         } finally {
             conn.close();
         }
@@ -95,24 +84,18 @@ public class TenantIDNDataDeletionUtil {
      * @param conn     database connection object
      * @param query    query for prepared statement
      * @param tenantId tenant id
-     * @throws Exception thrown if an error occurs while executing the query.
+     * @throws SQLException thrown if an error occurs while executing the query.
      */
     private static void executeDeleteQuery(Connection conn, String query, int tenantId)
-            throws Exception {
+            throws SQLException {
 
-        PreparedStatement ps = null;
-        try {
-            ps = conn.prepareStatement(query);
+        try (PreparedStatement ps = conn.prepareStatement(query)) {
             ps.setInt(1, tenantId);
             ps.executeUpdate();
         } catch (SQLException e) {
             String errMsg = "Error executing query " + query + " for tenant: " + tenantId;
             log.error(errMsg, e);
-            throw new Exception(errMsg, e);
-        } finally {
-            if (ps != null) {
-                ps.close();
-            }
+            throw new SQLException(errMsg, e);
         }
     }
 }
