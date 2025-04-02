9c8388d65899a20a76c0bf1aad3cf4c22a16ae49
Hasini Samarathunga <hasinis@wso2.com>
2025-04-02 12:40:52 +0530

    Introduce Tenant Name to Tenant Management API

--- components/tenant-mgt/org.wso2.carbon.tenant.mgt/src/main/java/org/wso2/carbon/tenant/mgt/util/TenantMgtUtil.java
+++ components/tenant-mgt/org.wso2.carbon.tenant.mgt/src/main/java/org/wso2/carbon/tenant/mgt/util/TenantMgtUtil.java
@@ -1,18 +1,21 @@
 /*
- * Copyright (c) 2008, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Copyright (c) 2008-2025, WSO2 LLC. (http://www.wso2.com).
+ *
+ * WSO2 LLC. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
+
 package org.wso2.carbon.tenant.mgt.util;
 
 import org.apache.axis2.clustering.ClusteringAgent;
@@ -521,8 +524,12 @@ public class TenantMgtUtil {
         TenantInfoBean bean = new TenantInfoBean();
         if (tenant != null) {
             bean.setTenantId(tenantId);
-            bean.setTenantDomain(tenant.getDomain());
             bean.setEmail(tenant.getEmail());
+
+            String domain = tenant.getDomain();
+            String name = tenant.getName();
+            bean.setTenantDomain(domain);
+            bean.setName(StringUtils.isNotBlank(name) ? name : domain);
 
             /*gets the created date*/
             Calendar createdDate = Calendar.getInstance();
