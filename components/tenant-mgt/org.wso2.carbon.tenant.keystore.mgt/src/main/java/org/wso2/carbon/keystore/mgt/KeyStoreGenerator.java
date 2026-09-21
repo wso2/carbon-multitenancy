0e49d5e4b17c77bba9e05e747245894e895f416b
sadilchamishka <sadil@wso2.com>
2026-09-21 12:09:17 +0530

    Remove non-conforming country attribute from tenant certificate DN

--- components/tenant-mgt/org.wso2.carbon.tenant.keystore.mgt/src/main/java/org/wso2/carbon/keystore/mgt/KeyStoreGenerator.java
+++ components/tenant-mgt/org.wso2.carbon.tenant.keystore.mgt/src/main/java/org/wso2/carbon/keystore/mgt/KeyStoreGenerator.java
@@ -152,7 +152,7 @@ public class KeyStoreGenerator {
             KeyPair keyPair = keyPairGenerator.generateKeyPair();
 
             // Common Name and alias for the generated certificate
-            String commonName = "CN=" + tenantDomain + ", OU=None, O=None, L=None, C=None";
+            String commonName = "CN=" + tenantDomain + ", OU=None, O=None, L=None";
 
             //generate certificates
             X500Name distinguishedName = new X500Name(commonName);
